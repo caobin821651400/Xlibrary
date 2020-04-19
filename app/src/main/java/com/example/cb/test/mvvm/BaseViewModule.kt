8e6180a9962138b752e8acfb1d47d72da7a0c8a5
caobin <821651400@qq.com>
2020-04-19 17:13:52 +0800

    1

--- app/src/main/java/com/example/cb/test/mvvm/BaseViewModule.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cb.test.mvvm
-
-interface BaseViewModule {
-
-     fun onDestriy()
-}
--- app/src/main/java/com/example/cb/test/mvvm/MvvmActivity.kt
+++ app/src/main/java/com/example/cb/test/mvvm/MvvmActivity.kt
@@ -3,33 +3,23 @@ package com.example.cb.test.mvvm
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.example.cb.test.R
-import com.example.cb.test.databinding.ActivityMvvmBinding
 
 /**
  * @author bin
  */
 class MvvmActivity : AppCompatActivity() {
 
-    private var mModule: MvvmModule? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_mvvm)
-
-        var binding: ActivityMvvmBinding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm)
-        mModule = MvvmModule(binding!!)
-
-        mModule!!.mViewDataBinding.user = mModule!!.setUserInfo(null)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mModule!!.onDestriy()
     }
 
     fun change(view: View) {
-        mModule!!.mViewDataBinding.user = mModule!!.setUserInfo("ttttt")
     }
 }
